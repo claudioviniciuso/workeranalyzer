93d3770af176c7681ad5a92feecd0af8eacbc849
Claudio Vinicius <claudiovinicius.o@hotmail.com>
2023-11-22 21:05:08 -0300

    fix: bug tests review

--- worker_analyzer/builders.py
+++ worker_analyzer/builders.py
@@ -1,6 +1,10 @@
 from datetime import datetime   
 
 class DefaultMetricsBuilder:
+    """
+    Default Metrics Builder is a class that helps build metrics based on counts.
+    It receives a status, stores it and at the end returns the metrics and status based on the counts per status.
+    """
     VALID_STATUSES = {'success', 'failure', 'blank'}
 
     def __init__(self, name) -> None:
@@ -11,35 +15,66 @@ class DefaultMetricsBuilder:
         self.start_time = None
         self.end_time = None
         self.duration = None
+        self.status = None
         self.total = 0
         self.success = 0
         self.failure = 0
         self.blank = 0
         self.errors = []
-        self.status = None
+        self.additional_metrics = {}
 
     @property
     def metrics(self):
         """ Returns a dictionary of the collected metrics. """
-        return {
+        self.metrics_dict = {
             "name": self.name,
             "duration": self.duration,
+            "status": self.status,
             "total": self.total,
             "success": self.success,
             "failure": self.failure,
             "blank": self.blank,
-            "errors": self.errors,
-            "status": self.status
+            "errors": self.errors
         }
+        self.metrics_dict.update(self.additional_metrics)
+        return self.metrics_dict
 
     def start(self):
         """ Marks the start time of the metric collection. """
         if self.start_time is not None:
             raise Exception("Metrics collection already started")
+        
         self.start_time = datetime.now()
     
+    def add_metrics_attr(self, metrics: dict):
+        """ Adds metrics to the metrics dictionary. """
+        if not isinstance(metrics, dict):
+            raise Exception("Metrics must be a dict")
+
+        if self.start_time is None:
+            raise Exception("Metrics collection must be started before adding metrics")
+
+        if self.end_time is not None:
+            raise Exception("Metrics collection already ended")
+
+        if len(metrics) == 0:
+            raise Exception("Metrics cannot be empty")
+
+        keys = metrics.keys()
+        for key in keys:
+            if key in self.metrics:
+                raise Exception(f"Metric '{key}' already exists")
+        
+        self.additional_metrics.update(metrics)
+
+
     def log(self, status, error_content=None):
         """ Logs a specific status and optional error content. """
+        if self.start_time is None:
+            raise Exception("Metrics collection must be started before logging")
+        if self.end_time is not None:
+            raise Exception("Metrics collection already ended")
+        status = status.lower()
         if status not in self.VALID_STATUSES:
             raise ValueError(f"Invalid status '{status}'. Valid statuses are: {self.VALID_STATUSES}")
 
@@ -68,12 +103,17 @@ class DefaultMetricsBuilder:
         except Exception as e:
             raise Exception(f"Error calculating duration: {e}")
 
-        if self.total == self.success:
+        if self.total == 0:
+            self.status = 'Not metrics logged'
+        elif self.total == self.success:
             self.status = 'success'
         elif self.total == self.failure:
             self.status = 'failure'
-        else:
+        elif self.total == self.blank:
             self.status = 'partial'
+        else:
+            self.status = 'Not started'
+
 
 class SimpleMetricsBuilder:
     def __init__(self, name) -> None:
